959fce720e457751f111838126b50e7e8ad2c057
Travis Vachon <travis.vachon@gmail.com>
2024-11-20 16:01:42 -0800

    fix: convert tabs to spaces

--- src/index.ts
+++ src/index.ts
@@ -17,67 +17,67 @@ import { createRefcode, createReferral, getRefcode, getReferredBy, listReferrals
 type RSRouterHandler = RouterHandler<Env>
 
 const referralCreate: RSRouterHandler = async function (
-	{ req, env }
+  { req, env }
 ) {
-	const form = await req.formData()
-	const email = form.get('email')?.toString()
-	const refcode = form.get('refcode')?.toString()
-	if (!email){
-		return new Response('invalid email', { status: 400 })
-	} else if (!refcode) {
-		return new Response('invalid refcode', { status: 400 })
-	} else {
-		await createReferral(env.REFERRALS, email, refcode)
-		return Response.json({})
-	}
+  const form = await req.formData()
+  const email = form.get('email')?.toString()
+  const refcode = form.get('refcode')?.toString()
+  if (!email){
+    return new Response('invalid email', { status: 400 })
+  } else if (!refcode) {
+    return new Response('invalid refcode', { status: 400 })
+  } else {
+    await createReferral(env.REFERRALS, email, refcode)
+    return Response.json({})
+  }
 }
 
 const referralList: RSRouterHandler = async function (
-	{ req, env }
+  { req, env }
 ) {
-	const refcode = decodeURIComponent(req.params.refcode)
-	return Response.json({
-		referrals: await listReferrals(env.REFERRALS, refcode)
-	})
+  const refcode = decodeURIComponent(req.params.refcode)
+  return Response.json({
+    referrals: await listReferrals(env.REFERRALS, refcode)
+  })
 }
 
 export const refcodeCreate: RSRouterHandler = async function (
-	{ req, env }
+  { req, env }
 ) {
-	const form = await req.formData()
-	const email = form.get('email')?.toString()
-	if (email) {
-		return Response.json({
-			refcode: await createRefcode(
-				env.REFERRALS,
-				email
-			)
-		})
-	} else {
-		return new Response('invalid email', { status: 400 })
-	}
+  const form = await req.formData()
+  const email = form.get('email')?.toString()
+  if (email) {
+    return Response.json({
+      refcode: await createRefcode(
+        env.REFERRALS,
+        email
+      )
+    })
+  } else {
+    return new Response('invalid email', { status: 400 })
+  }
 }
 
 const refcodeGet: RSRouterHandler = async function (
-	{ req, env }
+  { req, env }
 ) {
-	return Response.json({
-		refcode: await getRefcode(
-			env.REFERRALS,
-			decodeURIComponent(req.params.email)
-		)
-	})
+  return Response.json({
+    refcode: await getRefcode(
+      env.REFERRALS,
+      decodeURIComponent(req.params.email)
+    )
+  })
 }
 
 const referredByGet: RouterHandler = async function (
-	{ req, env }
+  { req, env }
 ) {
-	return Response.json({
-		refcode: await getReferredBy(
-			env.REFERRALS,
-			decodeURIComponent(req.params.email)
-		)
-	})
+  return Response.json({
+    refcode: await getReferredBy(
+      env.REFERRALS,
+      decodeURIComponent(req.params.email)
+    )
+  })
 }
 
 const router = new Router<Env, ExecutionContext, Request>()
@@ -89,32 +89,32 @@ router.get('/refcode/:email', refcodeGet)
 router.get('/referredby/:email', referredByGet)
 
 export async function calculateConversionsAndCredits (event: ScheduledController, env: Env, context: ExecutionContext) {
-	const result = await env.REFERRALS.prepare(
-		`SELECT email FROM referrals WHERE NOT rewarded`
-	).run()
-	if (result.error) {
-		throw result.error
-	} else {
-		const emails = result.results.map(r => r.email)
-		if (emails.length > 0) {
-			const stripe = new Stripe(env.STRIPE_API_KEY)
-			const query = "email: '" + emails.join("' OR email: '") + "'"
-			console.log(query)
-			const searchResult = await stripe.customers.search({ query })
-			console.log("STRIPE:", searchResult)
-		} else {
-			console.log("NOTHING TO CHECK")
-		}
-	}
+  const result = await env.REFERRALS.prepare(
+    `SELECT email FROM referrals WHERE NOT rewarded`
+  ).run()
+  if (result.error) {
+    throw result.error
+  } else {
+    const emails = result.results.map(r => r.email)
+    if (emails.length > 0) {
+      const stripe = new Stripe(env.STRIPE_API_KEY)
+      const query = "email: '" + emails.join("' OR email: '") + "'"
+      console.log(query)
+      const searchResult = await stripe.customers.search({ query })
+      console.log("STRIPE:", searchResult)
+    } else {
+      console.log("NOTHING TO CHECK")
+    }
+  }
 }
 
 export default {
-	async fetch (request, env, ctx): Promise<Response> {
-		router.cors({ allowOrigin: env.ALLOW_ORIGIN })
-		return router.handle(request, env, ctx)
-	},
+  async fetch (request, env, ctx): Promise<Response> {
+    router.cors({ allowOrigin: env.ALLOW_ORIGIN })
+    return router.handle(request, env, ctx)
+  },
 
-	async scheduled (event, env, ctx) {
-		ctx.waitUntil(calculateConversionsAndCredits(event, env, ctx))
-	},
+  async scheduled (event, env, ctx) {
+    ctx.waitUntil(calculateConversionsAndCredits(event, env, ctx))
+  },
 } satisfies ExportedHandler<Env>;
